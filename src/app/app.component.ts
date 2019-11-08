d3d26069229c172be97d9b1a1c411c8bb9a1ce75
BernardoMB <bmondragonbrozon@gmail.com>
2019-11-08 12:38:53 -0600

    Added some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'azure-media-services-in-angular';
+  title = "azure-media-services-in-angular";
+  currentMovie =
+    "https://courses-euwe.streaming.media.azure.net/0075ce84-4e42-4821-a65a-73056bfb7e5b/dbcef98449737251999287b0bea8e101.ism/manifest";
 }
